fbf4dbccb1444ee68ca92dc83f8d0378e7776173
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 16:47:50 +0530

    added flask cors

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -16,4 +18,4 @@ def upload_file():
         return 'File uploaded successfully'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
