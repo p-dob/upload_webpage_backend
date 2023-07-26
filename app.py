32c8fd159624bc1d00ac79305fc41a61709ea4e7
p-dob <88445292+p-dob@users.noreply.github.com>
2023-07-26 20:31:15 +0530

    zero bytes files solved

--- app.py
+++ app.py
@@ -15,36 +15,29 @@ def upload_file():
     if 'file' not in request.files:
         return 'No file part', 400
 
-    # Check if the request contains the 'webkitRelativePath' field.
-    if 'webkitRelativePath' in request.files['file']:
-        base_folder = 'uploaded_files'
-        file = request.files['file']
-        for filename in request.files.getlist('file'):
-            # Get the relative path of the file from the folder structure.
-            folder_structure = filename.webkit_relative_path.split('/')
-            # Sanitize folder names
-            sanitized_folder_structure = [sanitize_filename(folder_name) for folder_name in folder_structure]
-            # Get the folder name where the file should be saved.
-            folder_name = os.path.join(base_folder, *sanitized_folder_structure[:-1])
-            os.makedirs(folder_name, exist_ok=True)
+    base_folder = 'uploaded_files'
+    files = request.files.getlist('file')
 
-            # Save the file in the specified folder structure.
-            sanitized_filename = sanitize_filename(filename.filename)
-            file.save(os.path.join(folder_name, sanitized_filename))
-    else:
-        # Handle single file upload as before.
-        uploaded_file = request.files['file']
+    for file in files:
         folder_name = request.form.get('folderName')
         if folder_name:
-            # Save the file in the specified folder structure.
-            folder_path = f'uploaded_files/{sanitize_filename(folder_name)}'
-            os.makedirs(folder_path, exist_ok=True)
-            sanitized_filename = sanitize_filename(uploaded_file.filename)
-            uploaded_file.save(os.path.join(folder_path, sanitized_filename))
+            folder_path = f'{base_folder}/{sanitize_filename(folder_name)}'
         else:
-            # Save the file in the root folder.
-            sanitized_filename = sanitize_filename(uploaded_file.filename)
-            uploaded_file.save(f'uploaded_files/{sanitized_filename}')
+            folder_path = base_folder
+
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Save the file in the specified folder structure.
+        sanitized_filename = sanitize_filename(file.filename)
+        file_path = os.path.join(folder_path, sanitized_filename)
+        file.save(file_path)
+
+        # Check if the file was saved successfully and its size
+        if os.path.exists(file_path):
+            file_size = os.path.getsize(file_path)
+            print(f"Saved file: {file_path}, Size: {file_size} bytes")
+        else:
+            print(f"Failed to save file: {file_path}")
 
     return 'Files uploaded successfully'
 
