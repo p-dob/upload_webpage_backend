253c768928d0f9ba5f25fca7d69d20115a7244f0
p-dob <88445292+p-dob@users.noreply.github.com>
2023-08-08 01:23:37 +0530

    cors config change

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 
 def sanitize_filename(filename):
     # Get the file extension
